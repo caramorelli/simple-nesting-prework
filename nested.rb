bc312ad574797bbf159cd519384dcb8db9341d7c
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 01:39:47 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -64,11 +64,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
 
 	programmer_hash = 
  		{
@@ -85,6 +80,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
+  :yukihiro_matsumoto => {
+    :known_for => "Ruby",
+    :languages => ["LISP, C"]
+ }
 
     
 end
